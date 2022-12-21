56100aa5b39a0867e7f2d202e03787800c7a8d5e
Vit <you@example.com>
2022-12-21 14:10:19 +0200

    first commit1

--- jobs/settings.py
+++ jobs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lg0=gnrjdwbv0xv^x_ff=l=b#98k21o%z49&++&x3wy1hqbn!f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
